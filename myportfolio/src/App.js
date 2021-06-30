dcb7d36a248d6b0b9e17dccbebc6b629ecf3481a
Anaya Taylor <anayaetaylor@gmail.com>
2021-06-30 18:44:47 -0400

    working on home page

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,17 +1,18 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import chibime from './chibime_tp_crop_small.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={chibime} alt="me" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This will be the home for all my art/ craft projects.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/ataylor504/Portfolio"
           target="_blank"
           rel="noopener noreferrer"
         >
---
+++ myportfolio/src/Home.js
@@ -0,0 +1,16 @@
+import chibime from './chibime_tp_crop_small.png';
+
+function Home() {
+    return (
+      <div className="Home">
+        <header className="App-header">
+          <img src={chibime} alt="me" />
+          <p>
+            This will be the home for all my art/ craft projects.
+          </p>
+        </header>
+      </div>
+    );
+  }
+  
+  export default Home;
