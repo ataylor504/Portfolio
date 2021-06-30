8cbc6aea25a72cff9994aab667475e3733e118e0
Anaya Taylor <anayaetaylor@gmail.com>
2021-06-30 14:10:17 -0400

    started development

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Construction in Progress
         </a>
       </header>
     </div>
